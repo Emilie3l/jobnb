f31d3b3d924d1bfbe4c5e9e63113621de9182acc
Luis Zubia <soyzubia@gmail.com>
2020-11-18 14:08:47 -0700

    Apply to a job is done.

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,11 +1,24 @@
 class ApplicationsController < ApplicationController
-  before_action :set_application, only: [:new, :create]
+  before_action :set_job, only: [:new, :create]
 
   def new
+    @application = Application.new
+    @application.job = @job
+    @application.applicant = current_user
   end
   
   def create
+    if params[:application]
+      @application = Application.new(application_params)
+    else
+      @application = Application.new
+    end
+    
+    @application.job = @job
+    @application.applicant = current_user
+    
     if @application.save
+      flash[:notice] = "Your application has been submited. Have a nice day!"
       redirect_to job_path(@job)
     else
       render :new
@@ -15,13 +28,10 @@ class ApplicationsController < ApplicationController
   private
   
   def application_params
-    params.require('Parameters').permit(:cv)
+    params.require(:application).permit(:cv)
   end
   
-  def set_application
-    @application = Application.new
+  def set_job
     @job = Job.find(params[:job_id])
-    @application.job = @job
-    @application.applicant = current_user
   end
 end
--- app/models/application.rb
+++ app/models/application.rb
@@ -1,9 +1,8 @@
 class Application < ApplicationRecord
+  validates :cv, :job, :applicant, presence: true
   belongs_to :applicant, class_name: 'User'
   belongs_to :job
 
   # CV attachment
   has_one_attached :cv
-
-  validates :cv, presence: true
 end
