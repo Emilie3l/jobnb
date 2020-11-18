437504b30e9a8d4a3352abeeec7b639a92490f66
Luis Zubia <soyzubia@gmail.com>
2020-11-17 23:30:46 -0700

    Actions for jobs new, create, show, simple-index, edit, update and destroy, were implemented. Actions for applications, new and create, were implemented. If you go into the show for a jobs you can see the current applicants for that job. You can show a Job and Apply to it. Fixed some minor issues with Models references. Fixed some validations for creating Jobs. Main nav-bar was also modified, it shows the name of the user logged and the pop-up menu there is an actions for going to your profile and edit it.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# Create 15 jobs
+15.times do
+  forward_days = rand(10)
+  random_deadline = rand(100)*10 + forward_days
+  user = User.all.sample
+  job = Job.new
+  job.title = Faker::Job.title
+  job.description = Faker::Lorem.paragraphs
+  job.pay = Faker::Number.number(digits: 5)
+  job.start_date = Faker::Date.forward(days: forward_days)
+  job.deadline = Faker::Date.forward(days: forward_days + random_deadline)
+  job.employer = user
+  job.save
+end
